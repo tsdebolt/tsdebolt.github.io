fb8249d961d10c4286380939fd874cb4234d5b02
tsdebolt <70655858+tsdebolt@users.noreply.github.com>
2020-10-07 18:28:51 -0400

    Update golfScorecard.js

--- assignments/golfScorecard.js
+++ assignments/golfScorecard.js
@@ -72,8 +72,6 @@ let sub1 = (elem) => {
 let clr1 = (elem) => {
   elem.children[2].innerHTML = "-";
   elem.children[3].innerHTML = "-";
-  console.log(elem.children[2].innerHTML);
-  console.log(elem.children[3].innerHTML);
   totals();
 }
 
