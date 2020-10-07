8fa720e8cdeace3f4aa9e6f69df33fdf61c5fb12
tsdebolt <70655858+tsdebolt@users.noreply.github.com>
2020-10-07 18:20:12 -0400

    Update golfScorecard.js

--- assignments/golfScorecard.js
+++ assignments/golfScorecard.js
@@ -1,19 +1,24 @@
-let elem = []; 
+{let elem = []; 
 // assign the entire table row for all holes to a variable, elem 
+
 for (let i = 1; i < 19; i++){
   let s = i.toString();
     elem[i] = document.getElementById(s);
 }
 
-// assign a function to all + button
-for (let i = 1; i < 19; i++){
-elem[i].children[4].children[0].onclick 
-  = () => {add1(elem[i]);};
+// assign a function to all +, -, and clear buttons
+for (let i = 1; i < 19; i++) {
+ 
+  elem[i].children[4].children[0].onclick = () =>                  {add1(elem[i]);};
+  
+  elem[i].children[4].children[1].onclick = () =>                   {sub1(elem[i]);};
+  
+  elem[i].children[4].children[2].onclick = () =>                   {clr1(elem[i]);};
 }
 
-// create an "add1" function
+
+// create an "add1" function ----------------
 let add1 = (elem) => {
-  totals();
   
   if (elem.children[2].innerHTML == "-"){
     elem.children[3].innerHTML = 1 - parseInt(elem.children[1].innerHTML);
@@ -31,21 +36,14 @@ let add1 = (elem) => {
     elem.children[2].innerHTML = currentScore + 1;
     totals();
     }
-    
+     
     elem.children[3].innerHTML =   parseInt(elem.children[2].innerHTML) -parseInt(elem.children[1].innerHTML)
-  totals();
   }
 }
 
-// assign a function to all - button
-for (let i = 1; i < 19; i++){
-elem[i].children[4].children[1].onclick 
-  = () => {sub1(elem[i]);};
-}
 
-// create a "sub1" function
+// create a "sub1" function ---------------
 let sub1 = (elem) => {
-  totals();
   
   if (elem.children[2].innerHTML == "-"){
     elem.children[3].innerHTML = 1 - parseInt(elem.children[1].innerHTML);
@@ -66,17 +64,26 @@ let sub1 = (elem) => {
     } 
     
     elem.children[3].innerHTML = parseInt(elem.children[2].innerHTML) -parseInt(elem.children[1].innerHTML)
-    totals();
   }
 }
 
+
+// create a "clr1" function ---------------
+let clr1 = (elem) => {
+  elem.children[2].innerHTML = "-";
+  elem.children[3].innerHTML = "-";
+  console.log(elem.children[2].innerHTML);
+  console.log(elem.children[3].innerHTML);
+  totals();
+}
+
+
+// create a "totals" function ---------------
 let totals = () => {
   let pCount = 0;  
   let sCount = 0;
   let oCount = 0;
   for (let i = 1; i < 19; i++){
-    
-    
     if (elem[i].children[2].innerHTML !== "-"){
     pCount += parseInt(elem[i].children[1].innerHTML);
     sCount += parseInt(elem[i].children[2].innerHTML);
@@ -88,16 +95,5 @@ let totals = () => {
   document.getElementById("parTotal").innerHTML = pCount;
   document.getElementById("scoreTotal").innerHTML = sCount;
   document.getElementById("overTotal").innerHTML = oCount;
-  
 }
-/* your mission: 
-
-4. Make a table row's background color yellow if and only if that table row has a nonzero score.
-5. Make the HTML table row with id="totals" display appropriate totals. Totals should be computed only for holes that have yellow-highlighted nonzero scores. Like the yellow-highlighted table rows above, the "totals" table row background color must be yellow if and only if it has a nonzero score. 
-6. Add to the Action column a new button, C, which clears the current score for a given hole, and re-sets the table row background color to default.
-7. Replace HTML table row code with JavaScript code that appends table rows. That is, delete the HTML elements with ID's 1 through 18, and write a function that loops 18 times, appending the appropriate DOM elements with ID's 1 through 18.  
-8. Add a "RESET" button which clears all user-entered data. Use addEventListener method to add functionality to the button. See: https://www.w3schools.com/js/js_htmldom_eventlistener.asp. 
-9. Add an "ABOUT" button which displays the message, "Golf Scorecard 1.0. All rights reserved."  Use an arrow function to add the "ABOUT" button to the DOM. See: https://www.w3schools.com/Js/js_arrow_function.asp.
-10. Add a "FONT" button which toggles the font size of the entire app, from/to smaller to/from larger.
-*. Advanced (optional): put circles around scores that are birdies, and squares around scores that are bogeys!
-*/
+}
